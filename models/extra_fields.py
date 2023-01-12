4358e9c4005926003264070b157cbd629e77c4c6
Rodrigo Abrão Madureira <rodrigomadu85@gmail.com>
2023-01-12 16:19:04 -0300

    Corrigido Action Confirm

--- models/extra_fields.py
+++ models/extra_fields.py
@@ -1,9 +1,6 @@
 import datetime
-
-
 from odoo import fields, models, api, _
 from odoo.exceptions import UserError
-from datetime import date
 
 
 class HrEmployee(models.Model):
@@ -14,14 +11,14 @@ class HrEmployee(models.Model):
 class HrFields(models.Model):
     _inherit = "hr.attendance"
 
-    ordem_servico = apontamento = fields.Many2many('ordem.servico', 'hr_attendance_os_rel', 'hr_attendance_id', 'ordem_servico_id',
-                                   string='Linha Apontamento', store=True, copy=True)
+    ordem_servico = apontamento = fields.Many2many('ordem.servico', 'hr_attendance_os_rel', 'hr_attendance_id',
+                                                   'ordem_servico_id',
+                                                   string='Linha Apontamento', store=True, copy=True)
     valor_hora = fields.Float(string='Valor Hora', related='employee_id.valor_hora', readonly=True, store=True)
     retrabalho = fields.Boolean(string='Retrabalho', store=True)
     valor_total = fields.Float(string='Valor Total', readonly=True, store=True, compute='_total')
 
-
-    @api.depends('valor_hora','worked_hours','valor_total')
+    @api.depends('valor_hora', 'worked_hours', 'valor_total')
     def _total(self):
         for line in self:
             line.update({
@@ -29,11 +26,9 @@ class HrFields(models.Model):
             })
 
 
-
 class OsCertificados(models.Model):
     _name = "os.certificados"
     _description = "Certificados"
-
 
     data_criacao = fields.Date(string='Data', store=True, copy=True)
     certificado = fields.Many2many('ir.attachment', 'certificados_os_rel', 'arquivos_id', 'ir_attachment_id',
@@ -45,15 +40,14 @@ class OsFotos(models.Model):
     _name = "os.fotos"
     _description = "Fotos"
 
-
     data_criacao = fields.Date(string='Data', store=True, copy=True)
     certificado = fields.Many2many('ir.attachment', 'fotos_os_rel', 'os_fotos_id', 'ir_attachment_id',
                                    string='Certificado', store=True, copy=False)
 
+
 class OsPedidosCliente(models.Model):
     _name = "os.pedcli"
     _description = "Pedidos Cliente"
-
 
     data_criacao = fields.Date(string='Data', store=True, copy=True)
     pedido = fields.Many2many('ir.attachment', 'pedidos_os_rel', 'pedido_id', 'ir_attachment_id',
@@ -70,10 +64,14 @@ class OsDesenhos(models.Model):
                                string='Desenho', store=True, copy=True)
     os_arq = fields.Many2one('ordem.servico', string='Ordem de Serviço',
                              required=True, ondelete='cascade', index=True, copy=False)
-    tipo = fields.Selection([('Foto', 'Foto'), ('Peça', 'Peça'), ('Montagem', 'Montagem'), ('Plano de Corte', 'Plano de Corte')],
-                            string='Tipo', store=True, copy=True, required=True)
-    setor = fields.Selection([('Caldeiraria', 'Caldeiraria'), ('Equipamentos', 'Equipamentos'), ('Usinagem', 'Usinagem'), ('Outros', 'Outros')],
-                             string='Setor', store=True, copy=True, required=True)
+    tipo = fields.Selection(
+        [('Foto', 'Foto'), ('Peça', 'Peça'), ('Montagem', 'Montagem'), ('Plano de Corte', 'Plano de Corte')],
+        string='Tipo', store=True, copy=True, required=True)
+    setor = fields.Selection(
+        [('Caldeiraria', 'Caldeiraria'), ('Equipamentos', 'Equipamentos'), ('Usinagem', 'Usinagem'),
+         ('Outros', 'Outros')],
+        string='Setor', store=True, copy=True, required=True)
+
 
 # Campos adicionais(extra_fields)----------------------------------------------------------
 class OsPurchaseLine(models.Model):
@@ -81,19 +79,20 @@ class OsPurchaseLine(models.Model):
 
     ordem_servico = fields.Many2many('ordem.servico', 'purchase_order_line_os_rel', 'purchase_order_line_id', 'os_id',
                                      string='Pedidos de Compra', store=True, copy=True)
-   
 
     @api.model
     def _prepare_stock_move_vals(self, picking, price_unit, product_uom_qty, product_uom):
-
         vals = super(OsPurchaseLine, self)._prepare_stock_move_vals(picking, price_unit, product_uom_qty, product_uom)
         vals['ordem_servico'] = self.ordem_servico.ids
         return vals
-        
+
+
 class OsPurchase(models.Model):
     _inherit = "purchase.order"
-    
-    forma_pagamento = fields.Many2one('payment.acquirer', string='Forma de Pagamento', required=False, ondelete='restrict', index=True, copy=False)
+
+    forma_pagamento = fields.Many2one('payment.acquirer', string='Forma de Pagamento', required=False,
+                                      ondelete='restrict', index=True, copy=False)
+
 
 class OsStock(models.Model):
     _inherit = "stock.move"
@@ -101,12 +100,14 @@ class OsStock(models.Model):
     ordem_servico = fields.Many2many('ordem.servico', 'stock_move_rel_os', 'os_id', 'id',
                                      string='Ordem de Serviço', required=False, index=True, copy=False)
 
-class OsStockLine(models.Model):
 
+class OsStockLine(models.Model):
     _inherit = "stock.move.line"
     ordem_servico = fields.Many2many('ordem.servico', 'stock_move_line_os', 'os_id', 'id',
-                                     string='Ordem de Serviço', required=False, index=True, copy=False, related='move_id.ordem_servico')
+                                     string='Ordem de Serviço', required=False, index=True, copy=False,
+                                     related='move_id.ordem_servico')
     fornecedor = fields.Many2one(string='Fornecedor', related='picking_id.partner_id')
+
 
 class OsMrp(models.Model):
     _inherit = "mrp.production"
@@ -133,11 +134,10 @@ class OsSale(models.Model):
         required=True, change_default=True, index=True, tracking=1,
         domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]", )
 
-    @api.depends('valor_horas','materia_prima','terceiros')
+    @api.depends('valor_horas', 'materia_prima', 'terceiros')
     def _amount_resultado(self):
         for rec in self:
-            rec.update({'resultado' : rec.valor_horas + rec.materia_prima + rec.terceiros})
-
+            rec.update({'resultado': rec.valor_horas + rec.materia_prima + rec.terceiros})
 
     @api.model
     def _action_cancel(self):
@@ -152,23 +152,30 @@ class OsSale(models.Model):
     def _action_confirm(self):
 
         oss = self.ordem_servico.ids
+        name = self.name
         for os in oss:
             self.env['ordem.servico'].browse(os).write({'state': 'aberta'})
+
+
+        producao = self.env['mrp.production'].search([('origin', '=', name)])
+        producao.write({'ordem_servico': oss})
 
         res = super(OsSale, self)._action_confirm()
         return res
 
-class OsImpostos(models.Model):
 
+class OsImpostos(models.Model):
     _inherit = ["account.fiscal.position"]
 
-    meses = fields.Many2many('os.impostos.line', 'os_impostos_line_rel', 'os_impostos_line_id', 'impostos_id', string="Taxas", required=False, ondelete='cascade')
+    meses = fields.Many2many('os.impostos.line', 'os_impostos_line_rel', 'os_impostos_line_id', 'impostos_id',
+                             string="Taxas", required=False, ondelete='cascade')
 
 
 class OsImpostosLine(models.Model):
     _name = "os.impostos.line"
 
-    fiscal_position = fields.Many2one('account.fiscal.position', string="Posição Fiscal", default=lambda  self: self.id, store=True)
+    fiscal_position = fields.Many2one('account.fiscal.position', string="Posição Fiscal", default=lambda self: self.id,
+                                      store=True)
     mes = fields.Date(string='Data', store=True, copy=True, required=True)
     name = fields.Char(string='Name', store=True, compute='_compute_name')
     percentual = fields.Float(string='Percentual', store=True, required=True)
@@ -178,23 +185,24 @@ class OsImpostosLine(models.Model):
 
         for record in self:
             nome = datetime.date.strftime(record.mes, '%m/%Y')
-            busca = self.search([('name','=',nome)])
+            busca = self.search([('name', '=', nome)])
         if busca:
             raise UserError(_("Já existe lancamento no mesmo período"))
         else:
             for record in self:
                 record.name = datetime.date.strftime(record.mes, '%m/%Y')
 
+
 class OsNfs(models.Model):
     _name = "os.nfs"
-
 
     partner_id = fields.Many2one('res.partner', string='Fornecedor', translate=True, readonly=True, required=False,
                                  change_default=True, index=True, related='numero_nfe.partner_id')
     partner_id_saida = fields.Many2one('res.partner', string='Cliente', translate=True, readonly=True, required=False,
                                        change_default=True, index=True, related='nfe_saida.partner_id')
 
-    numero_nfe = fields.Many2many('l10n_br_fiscal.document', 'nfe_entrada_os_rel', 'account_move_id', 'ordem_servico_id',
+    numero_nfe = fields.Many2many('l10n_br_fiscal.document', 'nfe_entrada_os_rel', 'account_move_id',
+                                  'ordem_servico_id',
                                   string='Número NFE', copy=True, store=True, readonly=False)
     # data_emissao = fields.Datetime(string=u"Data da Emissão", translate=True, readonly=True, required=False,
     #                           change_default=True, index=True, tracking=1, related='numero.data_emissao')
@@ -203,5 +211,4 @@ class OsNfs(models.Model):
     #                                change_default=True, index=True, tracking=1,
     #                                related='numero.natureza_operacao')
     nfe_saida = fields.Many2many('l10n_br_fiscal.document', 'nfe_saida_os_rel', 'account_move_id', 'ordem_servico_id',
-                                  string='Número NFE', copy=True, store=True, readonly=False)
-
+                                 string='Número NFE', copy=True, store=True, readonly=False)
