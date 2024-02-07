cae11cf3ae932803bc202d0c406e909ce500528f
Rodrigo Abrão Madureira <rodrigomadu85@gmail.com>
2024-02-07 17:14:20 -0300

    Criado calculo de peso

--- models/products.py
+++ models/products.py
@@ -9,12 +9,35 @@ class CalcAttribute (models.Model):
 class ProdDimensions(models.Model):
     _inherit = "product.product"
 
-    peso_esp = fields.Float(string="Peso Específico Kg/m³", store=True, compute="_calc_pesoesp")
+    peso_esp = fields.Float(string='Peso Específico Kg/m³', store=True)
+    peso_calc = fields.Float(string='Peso Calculado', digits='Stock Weight', compute='_calcpeso')
 
+    @api.onchange('volume','peso_esp')
     def _calc_pesoesp(self):
         for rec in self:
             if rec.peso_esp:
-                rec.weight = rec.volume * rec.peso_esp
+                rec.peso_calc = rec.volume * rec.peso_esp
+                rec.weight = rec.peso_calc
+
+
+    def _calcpeso(self):
+
+        if self.product_width != 0 and self.product_height != 0 and self.product_length != 0 and self.volume==0:
+            self.volume = self.env["product.template"]._calc_volume(
+                self.product_length,
+                self.product_height,
+                self.product_width,
+                self.dimensional_uom_id,
+            )
+            self.peso_calc = self.volume * self.peso_esp
+            self.weight = self.peso_calc
+            exit()
+        if self.volume and self.peso_esp:
+            self.peso_calc = self.volume * self.peso_esp
+            self.weight = self.peso_calc
+        else:
+            self.peso_esp = 0
+
 
 
 class OsProduct(models.Model):
