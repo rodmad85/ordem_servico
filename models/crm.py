3d5db3239041f68a209bf4010b25fd4f454f58b2
rodrigo <rodrigomadu85@gmail.com>
2026-08-13 23:51:25 -0300

    [FIX]Corrigido campos crm

--- models/crm.py
+++ models/crm.py
@@ -4,5 +4,5 @@ from odoo import fields, models
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
-    medium_id = fields.Many2one(required=True)
-    tag_ids = fields.Many2many(required=True)
+    medium_id = fields.Many2one(required=True, ondelete="restrict")
+    tag_ids = fields.Many2many(required=True, ondelete="restrict")
